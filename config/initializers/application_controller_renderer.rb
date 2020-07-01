dfce7328aeb91312d8adf96fe26b9da10d074afe
Y-YOSHIMU <mcu7.oldtr.uc@gmail.com>
2020-07-01 11:48:49 +0900

    Initial Message

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'b3e06cc8fb193e69948cb8fb2ef8f712cb63bfc3af9a813dd724f5ec6954639cc851b3bceb53e990638707063fcdb3253615ebc83359662a2e434afc07cba985'
